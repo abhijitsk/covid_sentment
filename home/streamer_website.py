ac723886172d3a9df0fca20edc6df225fad956dc
Abhijit sasikumar <abhijit.cusat@gmail.com>
2021-08-31 10:30:28 +0100

    ariables set

--- home/streamer_website.py
+++ home/streamer_website.py
@@ -7,12 +7,12 @@ from .twitter_credentials import *
 #from twitter_credentials import *
 import pandas as pd
 from .streamer1 import *
-
+import os
 
 tweets_for_website = []
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token_key, access_token_secret)
+auth = tweepy.OAuthHandler(os.environ.get('consumer_key'), os.environ.get('consumer_secret'))
+auth.set_access_token(os.environ.get('access_token_key'), os.environ.get('access_token_secret'))
 
 api = tweepy.API(auth)
 for tweet in tweepy.Cursor(api.search, q = ("#corona"),count = 100, lang= 'en').items(100):
